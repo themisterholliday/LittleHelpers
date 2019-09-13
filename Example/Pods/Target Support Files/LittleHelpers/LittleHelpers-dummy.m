8eb769b96484e0f6d78a67b12deacc8c10cc4335
Craig Holliday <themisterholliday@gmail.com>
2019-09-13 15:32:25 -0600

    init

---
+++ Example/Pods/Target Support Files/LittleHelpers/LittleHelpers-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LittleHelpers : NSObject
+@end
+@implementation PodsDummy_LittleHelpers
+@end
---
+++ Example/Pods/Target Support Files/Pods-LittleHelpers_Example/Pods-LittleHelpers_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LittleHelpers_Example : NSObject
+@end
+@implementation PodsDummy_Pods_LittleHelpers_Example
+@end
